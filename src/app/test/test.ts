198e6ce2b3048c2f6e3000d8cc4b3bc26d9ddd6d
shivanandgn033 <shivanandgn033@gmail.com>
2026-01-16 13:53:20 +0530

    angular21-remove provide httpclient without defining in the appconfig

--- src/app/test/test.ts
+++ src/app/test/test.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { User } from '../user';
+import { OnInit } from '../../../node_modules/@angular/core/types/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './test.html',
   styleUrl: './test.scss',
 })
-export class Test {
+export class Test  implements OnInit {
 
+   userList :any;
+  constructor(private _user:User) {}
+
+  ngOnInit():void{
+    this.userData();
+  
+  }
+  userData(){
+this._user.getData().subscribe(data=>{
+  this.userList=data;
+  console.log(this.userList);
+  });
+}
 }
